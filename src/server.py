172e08f9ff06e889de18df192b7f94def8fa0360
Eloide Novela <eloide.novela@outlook.com>
2023-11-23 23:29:30 +0200

    Fiz algumas melhorias na rota create-dept

--- src/server.py
+++ src/server.py
@@ -1,26 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import mysql.connector
 from connection import myCursor
 from connection import db
 
 app = FastAPI()
+
 @app.post("/create-dept")
 async def add_dept(nome_departamento: str, nome_faculdade: str):
-    sql = 'select ver_dept(%s);'
-    value = (nome_departamento)
-    myCursor.execute(sql, value)
-    db.commit()
-    result = myCursor.fetchone()
+    if not nome_departamento or not nome_faculdade:
+        raise HTTPException(status_code=400, detail="Input invalido")
 
-    if (result == 1):
-        return {"Mensagem: ", "Departamento ja existente"}
-    
-    sql = "call set_dept(%s, %s);"
-    value = (nome_departamento, nome_faculdade)
-    myCursor.execute(sql, value)
-    db.commit()
-    return {"Mensagem:", "Sucesso"}
-    
-    
+    try:
+        sql = "call set_dept(%s, %s);"
+        value = (nome_departamento, nome_faculdade)
+        myCursor.execute(sql, value)
+        db.commit()
+        return {"Mensagem": "Sucesso"}
 
-
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        return {"Mensagem": "Departamento ja existente"}
