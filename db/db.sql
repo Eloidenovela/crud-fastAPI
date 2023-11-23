b169f4c28383247622be0aef4c3c67a76c7ab592
Eloide Novela <eloide.novela@outlook.com>
2023-11-23 23:08:40 +0200

    O script do backup do banco de dados, para reutiza-la basta so fazer um recover no seu sgbd

--- db/db.sql
+++ db/db.sql
@@ -1,1 +1,297 @@
-ECHO est  ativado.
+-- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: sigma
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `celldocente`
+--
+
+DROP TABLE IF EXISTS `celldocente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `celldocente` (
+  `idDocente` int(11) NOT NULL,
+  `celular` int(11) NOT NULL,
+  PRIMARY KEY (`idDocente`,`celular`),
+  CONSTRAINT `celldocente_ibfk_1` FOREIGN KEY (`idDocente`) REFERENCES `docente` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `celldocente`
+--
+
+LOCK TABLES `celldocente` WRITE;
+/*!40000 ALTER TABLE `celldocente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `celldocente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cellestudante`
+--
+
+DROP TABLE IF EXISTS `cellestudante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cellestudante` (
+  `idEstudante` int(11) NOT NULL,
+  `celular` int(11) NOT NULL,
+  PRIMARY KEY (`idEstudante`,`celular`),
+  CONSTRAINT `cellestudante_ibfk_1` FOREIGN KEY (`idEstudante`) REFERENCES `estudante` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cellestudante`
+--
+
+LOCK TABLES `cellestudante` WRITE;
+/*!40000 ALTER TABLE `cellestudante` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cellestudante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `curso`
+--
+
+DROP TABLE IF EXISTS `curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `curso` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(20) NOT NULL,
+  `Id_departamento` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk1` (`Id_departamento`),
+  CONSTRAINT `fk1` FOREIGN KEY (`Id_departamento`) REFERENCES `departamento` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `curso`
+--
+
+LOCK TABLES `curso` WRITE;
+/*!40000 ALTER TABLE `curso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `curso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `departamento`
+--
+
+DROP TABLE IF EXISTS `departamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `departamento` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(20) DEFAULT NULL,
+  `Nome_faculdade` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departamento`
+--
+
+LOCK TABLES `departamento` WRITE;
+/*!40000 ALTER TABLE `departamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `departamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `disciplina`
+--
+
+DROP TABLE IF EXISTS `disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `disciplina` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(20) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk2` (`id_curso`),
+  CONSTRAINT `fk2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `disciplina`
+--
+
+LOCK TABLES `disciplina` WRITE;
+/*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
+/*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `docente`
+--
+
+DROP TABLE IF EXISTS `docente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `docente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(30) NOT NULL,
+  `Apelido` varchar(30) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Senha` varchar(30) NOT NULL,
+  `CodDocente` int(11) NOT NULL,
+  `IdDisciplina` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Email` (`Email`),
+  UNIQUE KEY `CodDocente` (`CodDocente`),
+  KEY `IdDisciplina` (`IdDisciplina`),
+  CONSTRAINT `docente_ibfk_1` FOREIGN KEY (`IdDisciplina`) REFERENCES `disciplina` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `docente`
+--
+
+LOCK TABLES `docente` WRITE;
+/*!40000 ALTER TABLE `docente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `docente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estudante`
+--
+
+DROP TABLE IF EXISTS `estudante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estudante` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(30) NOT NULL,
+  `Apelido` varchar(30) NOT NULL,
+  `cod_Estudante` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cod_Estudante` (`cod_Estudante`),
+  UNIQUE KEY `email` (`email`),
+  KEY `id_curso` (`id_curso`),
+  CONSTRAINT `estudante_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudante`
+--
+
+LOCK TABLES `estudante` WRITE;
+/*!40000 ALTER TABLE `estudante` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estudante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exames`
+--
+
+DROP TABLE IF EXISTS `exames`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exames` (
+  `id_exame` int(11) NOT NULL AUTO_INCREMENT,
+  `id_disciplina` int(11) NOT NULL,
+  `exames` blob DEFAULT NULL,
+  `epoca` varchar(10) DEFAULT 'Normal',
+  `data` date DEFAULT NULL,
+  PRIMARY KEY (`id_exame`),
+  KEY `id_disciplina` (`id_disciplina`),
+  CONSTRAINT `exames_ibfk_1` FOREIGN KEY (`id_disciplina`) REFERENCES `disciplina` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exames`
+--
+
+LOCK TABLES `exames` WRITE;
+/*!40000 ALTER TABLE `exames` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exames` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material`
+--
+
+DROP TABLE IF EXISTS `material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `material` (
+  `id` int(11) NOT NULL,
+  `id_disciplina` int(11) NOT NULL,
+  `categoria` enum('Livro','Apostila','Ficha') DEFAULT 'Livro',
+  `Material` blob DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_disciplina` (`id_disciplina`),
+  CONSTRAINT `material_ibfk_1` FOREIGN KEY (`id_disciplina`) REFERENCES `disciplina` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material`
+--
+
+LOCK TABLES `material` WRITE;
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `testes`
+--
+
+DROP TABLE IF EXISTS `testes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `testes` (
+  `id_testes` int(11) NOT NULL AUTO_INCREMENT,
+  `id_disciplina` int(11) NOT NULL,
+  `testes` blob DEFAULT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id_testes`),
+  KEY `fk3` (`id_disciplina`),
+  CONSTRAINT `fk3` FOREIGN KEY (`id_disciplina`) REFERENCES `disciplina` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `testes`
+--
+
+LOCK TABLES `testes` WRITE;
+/*!40000 ALTER TABLE `testes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `testes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-23 20:02:30
