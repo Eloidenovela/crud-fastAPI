a122b0bffddbc7e9cc1c52acae718672d189dd65
Eloide Novela <eloide.novela@outlook.com>
2023-12-09 18:00:36 +0200

    Adicionei ja as rotas de update de delete

--- src/server.py
+++ src/server.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI, HTTPException
 import mysql.connector
-from connection import myCursor
-from connection import db
+from connection import db, myCursor
 
 app = FastAPI()
 
+# C
 @app.post("/create-dept")
 async def add_dept(nome_departamento: str, nome_faculdade: str):
     if not nome_departamento or not nome_faculdade:
         raise HTTPException(status_code=400, detail="Input invalido")
 
     try:
-        sql = "call set_dept(%s, %s);"
+        sql = "CALL SET_DEPT(%s, %s);"
         value = (nome_departamento, nome_faculdade)
         myCursor.execute(sql, value)
         db.commit()
@@ -21,23 +21,48 @@ async def add_dept(nome_departamento: str, nome_faculdade: str):
 
         return {"Erro: ", "{}".format(err)}
 
+# R
 @app.get("/read-dept")
 async def get_dept():
-    myCursor.execute("SELECT * FROM get_dept")
+    myCursor.execute("SELECT * FROM DEPARTAMENTO")
 
     result = myCursor.fetchall()
+    db.commit()
     data = []
 
     for row in result:
         row_data = {
-            "Nome-Departamento": row[0],
-            "Nome-Faculdade": row[1],
+            "Id-Departamento": row[0],
+            "Nome-Departamento": row[1],
+            "Nome-Faculdade": row[2],
         }
 
         data.append(row_data)
 
-    return {"data": data}
+    return {"Departamentos": data}
 
+# U
 @app.put("/update-dept")
-async def update_dept():
+async def update_dept(nome_departamento:str, nome_faculdade: str, id: int):
+    try:
+        sql = "CALL UPDATE_DEPT(%s, %s, %s);"
+        value = (nome_departamento, nome_faculdade, id)
+        myCursor.execute(sql, value)
+        db.commit()
+        return {"Sucesso": "Atualizacao feita com sucesso"}
+    except mysql.connector.Error as err:
+
+        return {"Erro: ", "{}".format(err)}
+
+# D
+@app.delete("/del-dept")
+async def delete_dept(id: int):
+    try:
+        sql = "CALL DEL_DEPT({});".format(id)
+        myCursor.execute(sql)
+        db.commit()
+        return {"Sucesso": "item removido!"}
     
+    except mysql.connector.Error as err:
+
+        return{"Erro: ", "{}".format(err)}
